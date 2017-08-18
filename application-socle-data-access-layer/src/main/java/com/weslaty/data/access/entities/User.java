c0ff64b5f66565f5f7eba3e614740c95c5b7f8ff
Ahmed Oueslati <weslaty@gmail.com>
2017-08-18 08:19:37 +0100

    Lombok integration

--- application-socle-data-access-layer/src/main/java/com/weslaty/data/access/entities/User.java
+++ application-socle-data-access-layer/src/main/java/com/weslaty/data/access/entities/User.java
@@ -1,11 +1,14 @@
 package com.weslaty.data.access.entities;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
+@Data
 public class User {
     @Id
     private Integer id;
@@ -30,91 +33,4 @@ public class User {
 
     private Date lastConnection;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobilePhone() {
-        return mobilePhone;
-    }
-
-    public void setMobilePhone(String mobilePhone) {
-        this.mobilePhone = mobilePhone;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(Date lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
-
-    public Date getLastConnection() {
-        return lastConnection;
-    }
-
-    public void setLastConnection(Date lastConnection) {
-        this.lastConnection = lastConnection;
-    }
 }
