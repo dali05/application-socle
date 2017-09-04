f204e38e75d173a14b7757005dc5701b468ffbfa
Ahmed Oueslati <weslaty@gmail.com>
2017-09-04 10:20:56 +0100

    Commit

--- application-socle-data-access-layer/src/main/java/com/weslaty/data/access/entities/User.java
+++ application-socle-data-access-layer/src/main/java/com/weslaty/data/access/entities/User.java
@@ -1,9 +1,12 @@
 package com.weslaty.data.access.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Past;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,6 +17,7 @@ public class User {
 
     private String login;
 
+    //@JsonIgnore
     private String password;
 
     private String firstName;
@@ -27,11 +31,15 @@ public class User {
     private Integer status;
 
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    @Past
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    @Past
     private LocalDateTime lastUpdateDate;
 
+    @Past
     private LocalDateTime lastConnection;
 
 }
